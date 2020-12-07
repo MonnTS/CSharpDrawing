9ab35116cbda80e5fea694630db26611d0909bfb
Daniel <73000732+Luf3r@users.noreply.github.com>
2020-12-07 20:23:37 +0300

    Add files via upload

---
+++ Laboratorium3/Laboratorium3/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Laboratorium3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Laboratorium3/Laboratorium3/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Laboratorium3
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        private string _name;
+        private string _city;
+        private string _date;
+
+        public string Name
+        {
+            get => _name;
+            set { _name = value; OnPropChanged("Name"); }
+        }
+
+        public string City
+        {
+            get => _city;
+            set { _city = value; OnPropChanged("City"); }
+        }
+
+        public string DateTime
+        {
+            get => _date;
+            set { _date = value; OnPropChanged("Date"); }
+        }
+
+        public Person(string name, string city, string dateTime)
+        {
+            _name = name;
+            _city = city;
+            _date = dateTime;
+        }
+    }
+}
